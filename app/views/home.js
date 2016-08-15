9fa19b602a2bccc4deb3ae692096b662503db30e
peters313 <eric.peters3@gmail.com>
2016-08-14 20:15:36 -0500

    modifies for single page app

--- app/views/home.js
+++ app/views/home.js
@@ -1,16 +1,1 @@
-var React = require('react');
 
-export default class Home extends React.Component({
-  render() {
-    return (
-      <div>
-        Hello, world! I am a CommentForm.
-      </div>
-    );
-  }
-});
-
-ReactDOM.render(
-   React.createElement(Home, null),
-  document.getElementbyId("container");
-);
